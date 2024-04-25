c3cc8a411ad60f925becf8cc8bf8496a6e368ca8
geumyoung <1gyn093@gmail.com>
2024-04-26 01:14:38 +0900

    server client에서 로그인 유무 보여주기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import * as actions from '@/actions';
+import { auth } from '@/auth';
 
-export default function Home() {
+export default async function Home() {
+	const session = await auth();
 	return (
 		<div>
 			<form action={actions.signIn}>
@@ -9,6 +11,11 @@ export default function Home() {
 			<form action={actions.signOut}>
 				<button>Sign Out</button>
 			</form>
+			<div>
+				{session?.user
+					? session?.user.name + '님 반갑습니다.'
+					: '로그인이 필요합니다.'}
+			</div>
 		</div>
 	);
 }
